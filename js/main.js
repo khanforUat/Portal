fbd150223a4e15b01bdba3c7334c896bc40c6d97
WFazal the II <zunairafazal51@gmail.com>
2024-09-20 03:28:30 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-analytics.js";
+import {
+    getAuth,
+    
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+import {
+    getFirestore
+    
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+
+const firebaseConfig = {
     apiKey: "AIzaSyCnY8Lf12kdWmDlHvdd32jYEjbb4aBcUIg",
     authDomain: "hakathon-test-df0a7.firebaseapp.com",
     projectId: "hakathon-test-df0a7",
@@ -14,8 +20,10 @@
     messagingSenderId: "983113037673",
     appId: "1:983113037673:web:b3eab90106e0cb4e0f6ff8",
     measurementId: "G-740P9F2EN2"
-  };
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+const db = getFirestore();
